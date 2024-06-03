89c71f10f98fc18b0fb68a92ca8d934171482205
EthanButterworth <136074583+EthanButterworth@users.noreply.github.com>
2024-06-03 18:32:00 +0100

    e

--- src/Components/Landing-Page/Landing-Page.js
+++ src/Components/Landing-Page/Landing-Page.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Landing-Page.css'
+import MyLogo from '../../Assets/My-Logo.png'
 
 const LandingPage = () => {
   return (
-    <div>Landing-Page</div>
+    <div>
+      <a href=''>
+        <img src={MyLogo} className='Logo'></img>
+      </a>
+      <h1>Butter-Log</h1>
+      <p>Log with the butter on.</p>
+    </div>
   )
 }
 
