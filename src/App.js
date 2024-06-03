937d5010b06a23693a1f19ddbc43fbc223fd912e
EthanButterworth <136074583+EthanButterworth@users.noreply.github.com>
2024-06-03 17:59:36 +0100

    e

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // src/App.js
 import React from 'react';
 import './App.css';
+import LandingPage from './Components/Landing-Page/Landing-Page.js'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <LandingPage />
       </header>
     </div>
   );
