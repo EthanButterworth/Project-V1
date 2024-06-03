cd23436a0f30d2e32a4c025fd31cbbaf0d4dc9b9
EthanButterworth <136074583+EthanButterworth@users.noreply.github.com>
2024-06-03 15:49:28 +0100

    e

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import BurgerMenu from './components/BurgerMenu';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BurgerMenu />
+
       </header>
     </div>
   );
